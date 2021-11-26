ae4c9f37ef1ad27de08d1fdea10d5a9aff4ee998
fredes <ricardofredes@gmail.com>
2021-11-26 12:04:09 -0300

    feat: add password field

---
+++ src/forms/PasswordField/password-field.component.tsx
@@ -0,0 +1,26 @@
+import { PasswordFieldType, PasswordIconName, PasswordFieldProps } from './password-field.types'
+
+import { useState } from "react";
+import TextField from '../TextField/text-field.component';
+
+const PasswordField = (props: PasswordFieldProps) => {
+  const [type, setType] = useState(PasswordFieldType.Hidden)
+
+  const isVisible = type === PasswordFieldType.Hidden
+  
+  const  toggleReveal = () => {
+    if (props.disabled) return
+    isVisible ? 
+      setType(PasswordFieldType.Visible) : 
+      setType(PasswordFieldType.Hidden)
+  }
+  
+  const rightIconButton = {
+    name: isVisible ? PasswordIconName.Visible : PasswordIconName.Hidden,
+    onClick: toggleReveal
+  }
+
+  return <TextField {...props}  rightIconButton={rightIconButton} type={type}/>
+}
+
+export default PasswordField

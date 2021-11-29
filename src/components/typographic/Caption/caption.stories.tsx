1155f57b1ea69f02c2ba07bc22d3af3b30b3dfb4
fredes <ricardofredes@gmail.com>
2021-11-29 16:57:16 -0300

    feat: add caption component

---
+++ src/components/typographic/Caption/caption.stories.tsx
@@ -0,0 +1,14 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import Caption from "./caption.component";
+
+export default {
+  title: "Typographic/Caption",
+  component: Caption,
+} as ComponentMeta<typeof Caption>;
+
+const Template: ComponentStory<typeof Caption> = (args) => <Caption {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Caption"
+};

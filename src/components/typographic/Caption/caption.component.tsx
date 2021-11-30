c23dca1e1d67c55a9b44a98763a6b7c8d520624a
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2021-11-30 15:51:39 -0300

    fix: fixed merge

--- src/components/typographic/Caption/caption.component.tsx
+++ src/components/typographic/Caption/caption.component.tsx
@@ -3,11 +3,7 @@ import type { CaptionProps } from "./caption.types";
 
 const Caption = ({ as, children, className, ...props }: CaptionProps) => {
   const SubtitleTag = as || "caption";
-<<<<<<< HEAD
   const cn = classNames("caption", className);
-=======
-  const cn = classNames("caption", classNames);
->>>>>>> 0c9a089 (style: run linter)
   return (
     <SubtitleTag className={cn} {...props}>
       {children}

c1aac4ed58a79f4937de9f8120fcadb032362bee
fredes <ricardofredes@gmail.com>
2021-11-30 09:38:53 -0300

    refactor(card): remove eslint comment

--- src/components/Card/card.component.tsx
+++ src/components/Card/card.component.tsx
@@ -3,7 +3,6 @@ import type { CardProps } from "./card.types";
 import classNames from "classnames";
 
 const Card = ({ children, elevation, className, ...props }: CardProps) => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   const cn = classNames("card", { [`card--elevation-${elevation}`]: elevation }, className);
   return (
     <div className={cn} {...props}>

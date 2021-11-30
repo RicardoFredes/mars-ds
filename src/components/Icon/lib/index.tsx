fd75b873c72e62d8d2f817cdf6dbe6d546b2fd3a
fredes <ricardofredes@gmail.com>
2021-11-30 09:53:27 -0300

    feat: add chevron-down icon

--- src/components/Icon/lib/index.tsx
+++ src/components/Icon/lib/index.tsx
@@ -7,14 +7,16 @@ import eyeOff from "./eye-off";
 import mesalvaMini from "./me-salva-mini";
 import ppl from "./ppl";
 import tri from "./tri";
-import info from './info'
-import shield from './shield'
+import info from "./info";
+import shield from "./shield";
+import chevronDown from "./chevron-down";
 
 const icons: {
   [key: string]: (props: IconLibProps) => JSX.Element;
 } = {
   "alert-circle": alertCircle,
   "checkmark-circle": checkmarkCircle,
+  "chevron-down": chevronDown,
   "eye-off": eyeOff,
   eye,
   info,

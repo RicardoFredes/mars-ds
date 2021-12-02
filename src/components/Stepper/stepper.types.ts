446c86ba91ec85134b43491dff5885f2c2689832
fredes <ricardofredes@gmail.com>
2021-12-02 14:02:12 -0300

    refactor: fix stepper layout

--- src/components/Stepper/stepper.types.ts
+++ src/components/Stepper/stepper.types.ts
@@ -2,5 +2,5 @@ import type { ReactElement } from "react";
 
 export interface StepperProps extends ReactElement<HTMLElement> {
   steps: Array<string>;
-  position: number;
+  currentPosition: number;
 }

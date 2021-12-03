5db5658243e2264fb09de992db038c677025d0b8
Rafael Julio <rafael.julio@mesalva.com>
2021-12-03 19:23:03 -0300

    chore: remove useless type
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- types/jsx.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DetailedHTMLProps, HTMLAttributes } from "react";
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      div: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
-    }
-  }
-}

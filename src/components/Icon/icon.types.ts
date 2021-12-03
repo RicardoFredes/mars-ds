0c6142c741375b78012b42396ce6b8707430e2ea
Rafael Julio <rafael.julio@mesalva.com>
2021-12-03 19:53:25 -0300

    refactor: enforce literal name for icon type
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- src/components/Icon/icon.types.ts
+++ src/components/Icon/icon.types.ts
@@ -1,12 +1,14 @@
 import type { Sizes } from "@/types";
 import type { HTMLProps } from "react";
 
+import IconLibrary from "./lib";
+
 export interface IconLibProps {
   height?: string | number;
   width?: string | number;
 }
 
-export interface IconProps extends Omit<HTMLProps<HTMLSpanElement>, "size"> {
+export interface IconProps extends Omit<HTMLProps<HTMLSpanElement>, "name" | "size"> {
   size?: Sizes;
-  name: string;
+  name: keyof typeof IconLibrary;
 }
--- src/components/IconButton/icon-button.types.ts
+++ src/components/IconButton/icon-button.types.ts
@@ -1,5 +1,6 @@
+import type { IconProps } from "@/components/Icon/icon.types";
 import type { ButtonProps } from "../Button/button.types";
 
 export interface IconButtonProps extends ButtonProps {
-  iconName: string;
+  iconName: IconProps["name"];
 }

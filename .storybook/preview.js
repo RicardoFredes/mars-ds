beb319a976b72ab45a216643d3dc4839a595f510
fredes <ricardofredes@gmail.com>
2021-12-10 16:53:57 -0300

    chore: add storybook customizations

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,4 +13,24 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#f4f6f8',
+      },
+      {
+        name: 'dark',
+        value: '#919eab29',
+      },
+    ],
+    grid: {
+      cellSize: 8,
+      opacity: 0.25,
+      cellAmount: 10,
+      offsetX: 16, // default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
+      offsetY: 16, // default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
+    },
+  },
 };

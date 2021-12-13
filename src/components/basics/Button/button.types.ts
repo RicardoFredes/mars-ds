0d6c6167bb74218fb7fa90cb2a1a12049ed236ed
fredes <ricardofredes@gmail.com>
2021-12-13 13:42:59 -0300

    refactor(button): add custom types

--- src/components/basics/Button/button.types.ts
+++ src/components/basics/Button/button.types.ts
@@ -1,9 +1,22 @@
-import type { Sizes, Variants } from "@/types";
+import { SizesEnum, VariantsEnum } from "@/types";
+
+const { Small, Medium } = SizesEnum;
+
+export const ButtonSizesEnum = {
+  Medium,
+  Small,
+} as const;
+
+export const ButtonVariantsEnum = VariantsEnum;
+
+export type ButtonSizes = typeof ButtonSizesEnum[keyof typeof ButtonSizesEnum];
+
+export type ButtonVariants = typeof ButtonVariantsEnum[keyof typeof ButtonVariantsEnum];
 
 export interface ButtonProps
   extends Omit<React.HTMLProps<HTMLButtonElement | HTMLLinkElement>, "as" | "size"> {
   as?: JSX.Element | string;
-  variant?: Variants;
+  variant?: ButtonVariants;
   label?: string;
-  size?: Sizes;
+  size?: ButtonSizes;
 }

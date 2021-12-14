946f469bd8120f2710fd6b3ec002d552d8a52b24
Rafael Julio <rafael.julio@mesalva.com>
2021-12-14 16:22:41 -0300

    chore: fix eslint config
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,42 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    es6: true,
-    jest: true,
     node: true,
+    es6: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
-    "plugin:prettier/recommended",
-  ],
-  settings: {
-    react: {
-      version: "detect",
-    },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
   },
   overrides: [
     {
-      files: ["**/*.stories.*"],
-      rules: {
-        "import/no-anonymous-default-export": "off",
+      files: ["src/**/*.ts?(x)"],
+      env: {
+        browser: true,
+        jest: true,
       },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "plugin:storybook/recommended",
+        "plugin:prettier/recommended",
+      ],
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      overrides: [
+        {
+          files: ["**/*.stories.*"],
+          rules: {
+            "import/no-anonymous-default-export": "off",
+          },
+        },
+      ],
     },
   ],
 };

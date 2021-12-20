47232e0c6338aaf39ee9008909de28e503ec7669
fredes <ricardofredes@gmail.com>
2021-12-20 09:38:50 -0300

    feat: add avatar component

--- src/components/basics/Avatar/avatar.component.tsx
+++ src/components/basics/Avatar/avatar.component.tsx
@@ -1,13 +1,45 @@
-import type { AvatarProps } from "./avatar.types";
+import { AvatarProps, AvatarSizes } from "./avatar.types";
 import classNames from "classnames";
+import { useMemo } from "react";
+import { ItemText } from "@/index";
 
-const Avatar = ({ className, children, ...props }: AvatarProps) => {
-  const cn = classNames("avatar", className);
+const Avatar = ({
+  className,
+  children,
+  name,
+  thumbnail,
+  size = AvatarSizes.Medium,
+  style,
+  ...props
+}: AvatarProps) => {
+  const cn = classNames("avatar", className, `avatar--size-${size}`);
+  const initials = useMemo(() => getLettersByName(name) || "?", [name]);
+  const styleComputed = useMemo(
+    () => ({ ...style, backgroundImage: thumbnail && `url(${thumbnail})` }),
+    [thumbnail, style]
+  );
   return (
-    <div className={cn} {...props}>
+    <div className={cn} style={styleComputed} {...props}>
       {children}
+      {!thumbnail && <ItemText className="avatar__initials">{initials}</ItemText>}
     </div>
   );
 };
 
 export default Avatar;
+
+export const getLettersByName = (name?: string) => {
+  if (!name) return "";
+  const getLetter = (letter?: string) => letter?.toLocaleUpperCase() || "";
+  let result = getLetter(name[0]);
+  let getNext = false;
+  for (let i = 1; result.length < 2 && i < name.length; i++) {
+    const letter = name[i] || "";
+    if (letter === " ") {
+      getNext = true;
+      continue;
+    }
+    if (getNext) result += getLetter(letter);
+  }
+  return result;
+};
--- src/components/basics/Avatar/avatar.types.ts
+++ src/components/basics/Avatar/avatar.types.ts
@@ -1,1 +1,10 @@
-export type AvatarProps = React.HTMLProps<HTMLDivElement>;
+import { Sizes } from "@/types";
+
+const { XSmall, Small, Medium, Large, XLarge } = Sizes;
+export const AvatarSizes = { XSmall, Small, Medium, Large, XLarge };
+
+export interface AvatarProps extends Omit<React.HTMLProps<HTMLDivElement>, "size"> {
+  name?: string;
+  thumbnail?: string;
+  size?: typeof AvatarSizes[keyof typeof AvatarSizes];
+}

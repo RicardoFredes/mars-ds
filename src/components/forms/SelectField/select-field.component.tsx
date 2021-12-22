110fba4eb2f757de2c6d76381a49676ef72ab494
fredes <ricardofredes@gmail.com>
2021-12-22 09:25:28 -0300

    refactor: select-filed

--- src/components/forms/SelectField/select-field.component.tsx
+++ src/components/forms/SelectField/select-field.component.tsx
@@ -72,6 +72,7 @@ const SelectField = ({
     window.history.pushState("select__field--is-openned", "select");
     addEventListener();
     setIsOpen(true);
+    handleSetIsAbove();
   };
 
   const handleSetIsAbove = () => {
@@ -87,10 +88,7 @@ const SelectField = ({
     setIsDropdownAbove(hasSpaceAbove);
   };
 
-  useEffect(() => {
-    if (isOpen) handleSetIsAbove();
-  }, [isOpen]);
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => close, []);
 
   const getElementOptionItems = () => {

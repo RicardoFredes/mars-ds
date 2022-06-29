6445e469aabfa1c620d6696c00f24bf300d332d3
Fredes <ricardofredes@gmail.com>
2022-06-29 12:09:08 -0300

    chore: bump versions (#176)
    * chore: remove dompurify
    
    * chore: bump storybook
    
    * chore: bump more versions
    
    * chore: run audit

--- .storybook/main.js
+++ .storybook/main.js
@@ -22,4 +22,5 @@ module.exports = {
       },
     };
   },
+  framework: "@storybook/react",
 };
--- src/services/html.ts
+++
@@ -1,6 +0,0 @@
-import DOMPurify from "dompurify";
-
-export const sanitizeHTML = (html: string) => {
-  if (typeof window === "undefined") return html;
-  return DOMPurify.sanitize(html);
-};

a57c23500f5110ca8ef3670c139eb8bbb0460824
paulocjota <74553160+paulocjota@users.noreply.github.com>
2022-09-28 09:42:08 -0300

    refactor: allow sanitize helper preserve target attribute (#214)

--- src/services/sanitize.spec.ts
+++ src/services/sanitize.spec.ts
@@ -5,4 +5,28 @@ describe("sanitize", () => {
     expect(sanitize("<img src=x onerror=alert(1)//>")).toEqual('<img src="x">');
     expect(sanitize("<svg><g/onload=alert(2)//<p>")).toEqual("<svg><g></g></svg>");
   });
+
+  it('should automatically add attribute rel="noopener" in anchors with target="_blank"', () => {
+    expect(
+      sanitize('<a role="anchor" href="https://www.google.com" target="_blank">hello</a>').includes(
+        'rel="noopener"'
+      )
+    ).toBe(true);
+    expect(
+      sanitize('<a href="https://www.google.com" target="_blank" rel="any">hello</a>').includes(
+        'rel="noopener"'
+      )
+    ).toBe(true);
+  });
+
+  it('should NOT automatically add attribute rel="noopener" in anchors without target="_blank"', () => {
+    expect(
+      sanitize('<a role="anchor" href="https://www.google.com">hello</a>').includes(
+        'rel="noopener"'
+      )
+    ).toBe(false);
+    expect(
+      sanitize('<a href="https://www.google.com" rel="any">hello</a>').includes('rel="noopener"')
+    ).toBe(false);
+  });
 });
--- src/services/sanitize.ts
+++ src/services/sanitize.ts
@@ -1,5 +1,15 @@
 import DOMPurify from "dompurify";
 
+DOMPurify.setConfig({ ADD_ATTR: ["target"] });
+
+DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+  if (node.tagName === "A") {
+    if (node.getAttribute("target") === "_blank") {
+      node.setAttribute("rel", "noopener");
+    }
+  }
+});
+
 export const sanitize = (html: string) => {
   return DOMPurify.sanitize(html);
 };

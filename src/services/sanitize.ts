5cfbfbe1dd8e302ca5fcf3e498d4ada16daa29be
fredes <ricardofredes@gmail.com>
2022-09-30 17:11:55 -0300

    chore(sanitize): add try catch

--- src/services/sanitize.ts
+++ src/services/sanitize.ts
@@ -1,15 +1,19 @@
 import DOMPurify from "dompurify";
 
-DOMPurify.setConfig({ ADD_ATTR: ["target"] });
-
-DOMPurify.addHook("afterSanitizeAttributes", function (node) {
-  if (node.tagName === "A") {
-    if (node.getAttribute("target") === "_blank") {
-      node.setAttribute("rel", "noopener");
-    }
-  }
-});
-
 export const sanitize = (html: string) => {
-  return DOMPurify.sanitize(html);
+  try {
+    DOMPurify.setConfig({ ADD_ATTR: ["target"] });
+
+    DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+      if (node.tagName === "A") {
+        if (node.getAttribute("target") === "_blank") {
+          node.setAttribute("rel", "noopener");
+        }
+      }
+    });
+
+    return DOMPurify.sanitize(html);
+  } catch (error) {
+    return html;
+  }
 };
